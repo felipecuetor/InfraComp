5ef928d97768f16d396229317a3dca5e3d0dcc82
Felipe Cueto <f.cueto10@uniandes.edu.co>
2016-02-27 22:30:01 -0500

    Buffer

--- src/Servidor/Buffer.java
+++ src/Servidor/Buffer.java
@@ -1,6 +1,41 @@
 package Servidor;
 
+import java.util.ArrayList;
+import java.util.Queue;
+
+import Cliente.Mensaje;
+
 public class Buffer {
+	private Queue<Mensaje> porProcesar;
+	
+	private int capacidad;
+	
+	private int contador;
+	
+	public synchronized boolean recibirMensaje(Mensaje mensaje)
+	{
+		if(contador == capacidad)
+			return false;
+		porProcesar.add(mensaje);
+		contador++;
+		notify();
+		return true;
+	}
+	
+	public synchronized Mensaje atender()
+	{
+		if(contador == 0)
+		{
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return porProcesar.poll();
+		
+	}
 
 	public Buffer(int parseInt) {
 		// TODO Auto-generated constructor stub
